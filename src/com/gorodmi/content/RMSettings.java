5b23c060b5fa8047c3abf0d049cd60b74831431a
Mythril382 <77225817+Mythril382@users.noreply.github.com>
2022-06-24 17:53:20 +0800

    Update RMSettings.java

--- src/com/gorodmi/content/RMSettings.java
+++ src/com/gorodmi/content/RMSettings.java
@@ -1,3 +1,21 @@
 package com.gorodmi.content;
 
-//im editing this
+import arc.*;
+import arc.util.*;
+import mindustry.*;
+import mindustry.gen.*;
+import mindustry.ui.*;
+import mindustry.ui.dialogs.*;
+
+import static arc.Core.*;
+import static mindustry.Vars.*;
+
+public class RMSettings{
+    public void load(){
+        SettingsMenuDialog dialog = ui.settings;
+        
+        dialog.addCategory("@setting.rm", Icon.effect /* TODO */, c -> {
+            c.checkPref("rm-ammo", true);
+        });
+    }
+}
