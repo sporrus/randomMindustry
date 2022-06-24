0bbbdf8c056a17983ea8429957b8b1948e4353a2
Mythril382 <77225817+Mythril382@users.noreply.github.com>
2022-06-25 03:36:01 +0800

    move item drop stuff

--- src/randomMindustry/BlockMapper.java
+++ src/randomMindustry/BlockMapper.java
@@ -6,31 +6,17 @@ import mindustry.content.*;
 import mindustry.type.*;
 import mindustry.world.*;
 import mindustry.world.blocks.production.*;
-import mindustry.world.blocks.environment.*;
 
 import static mindustry.Vars.*;
 
 public class BlockMapper{
-    // TODO: separate for both planets?
-    static Seq<Block> tier1, tier2, tier3, tier4, tier5;
-    
     public static void init(){
         Seq<Block> blocks = content.blocks().copy();
-        
-        tier1 = new Seq<>(blocks.select(b -> b instanceof OreBlock && b.itemDrop.hardness == 1));
-        tier2 = new Seq<>(blocks.select(b -> b instanceof OreBlock && b.itemDrop.hardness == 2));
-        tier3 = new Seq<>(blocks.select(b -> b instanceof OreBlock && b.itemDrop.hardness == 3));
-        tier4 = new Seq<>(blocks.select(b -> b instanceof OreBlock && b.itemDrop.hardness == 4));
-        tier5 = new Seq<>(blocks.select(b -> b instanceof OreBlock && b.itemDrop.hardness == 5));
-        
         blocks.shuffle();
         blocks.each(BlockMapper::modify);
     }
 
     public static void modify(Block block){
-        // ores dont have nodes so im putting this on the front
-        if (block instanceof OreBlock) modifyOre((OreBlock)block);
-        
         TechTree.TechNode node = block.techNode;
         if(node == null) return;
         while (node.parent != null) node = node.parent;
@@ -54,35 +40,5 @@ public class BlockMapper{
 
     public static void modifyBlock(Block block) {
         block.requirements = ResourceMapper.getRandomItemStacks(ResourceMapper.getRandomInt(5)+1, 5, 1000, true);
-    }
-    
-    public static void modifyOre(OreBlock block) {
-        switch(block.itemDrop.hardness) {
-            case 1:
-                Block t1 = tier1.random();
-                tier1.remove(t1);
-                block.itemDrop = t1.itemDrop;
-                break;
-            case 2:
-                Block t2 = tier2.random();
-                tier2.remove(t2);
-                block.itemDrop = t2.itemDrop;
-                break;
-            case 3:
-                Block t3 = tier3.random();
-                tier3.remove(t3);
-                block.itemDrop = t3.itemDrop;
-                break;
-            case 4:
-                Block t4 = tier4.random();
-                tier4.remove(t4);
-                block.itemDrop = t4.itemDrop;
-                break;
-            case 5:
-                Block t5 = tier5.random();
-                tier5.remove(t5);
-                block.itemDrop = t5.itemDrop;
-                break;
-        }
     }
 }
