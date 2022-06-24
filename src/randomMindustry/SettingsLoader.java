beee6b20c96da531d1722f3b78921886b725852f
Mythril382 <77225817+Mythril382@users.noreply.github.com>
2022-06-24 23:29:38 +0800

    Update SettingsLoader.java

--- src/randomMindustry/SettingsLoader.java
+++ src/randomMindustry/SettingsLoader.java
@@ -9,10 +9,8 @@ public class SettingsLoader{
     public static void init(){
         SettingsMenuDialog dialog = ui.settings;
         
-        dialog.addCategory("@setting.rm", Icon.effect /* TODO */, c -> {
-            c.checkPref("rm-ammo", true);
-            c.checkPref("rm-block-output", true);
-            c.checkPref("rm-block-input", true);
+        dialog.addCategory("@setting.rm", Icon.effect /* TODO: Make custom icons. */, c -> {
+            // TODO: Add settings.
         });
     }
 }
