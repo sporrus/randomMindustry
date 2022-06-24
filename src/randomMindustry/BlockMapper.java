350fad42b21e8030ea4a18b8113e25909b64181e
Mythril382 <77225817+Mythril382@users.noreply.github.com>
2022-06-25 01:31:20 +0800

    Update BlockMapper.java

--- src/randomMindustry/BlockMapper.java
+++ src/randomMindustry/BlockMapper.java
@@ -7,23 +7,26 @@ import mindustry.type.*;
 import mindustry.world.*;
 import mindustry.world.blocks.production.*;
 
-public class BlockMapper {
-    public static void init() {
+public class BlockMapper{
+    public static void init(){
         Seq<Block> blocks = Vars.content.blocks().copy();
         blocks.shuffle();
         blocks.each(BlockMapper::modify);
     }
 
-    public static void modify(Block block) {
+    public static void modify(Block block){
         TechTree.TechNode node = block.techNode;
-        if (node == null) return;
+        if(node == null) return;
         while (node.parent != null) node = node.parent;
-        if (Planets.serpulo.techTree != node) return;
-        modifyBlock(block);
-        if (block instanceof GenericCrafter) modifyCrafter((GenericCrafter) block);
+        if(Planets.serpulo.techTree != node) return;
+        if(block instanceof GenericCrafter){
+            modifyCrafter((GenericCrafter)block);
+        }else{
+            modifyBlock(block);
+        }
     }
 
-    public static void modifyCrafter(GenericCrafter block) {
+    public static void modifyCrafter(GenericCrafter block){
         Item item = ResourceMapper.getCraftItem();
         if (item == null) item = ResourceMapper.getAnyItem();
         int count = ResourceMapper.getRandomInt(10)+1;
@@ -32,7 +35,7 @@ public class BlockMapper {
         )};
     }
 
-    public static void modifyBlock(Block block) {
+    public static void modifyBlock(Block block){
         block.requirements = ResourceMapper.getRandomStack(ResourceMapper.getRandomInt(2)+1, 5, 1000, true);
     }
 }
