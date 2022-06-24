1ca6b37472b90a38499f59b39d56e0a95839b40c
Mythril382 <77225817+Mythril382@users.noreply.github.com>
2022-06-24 20:38:06 +0800

    Update Main.java

--- src/com/gorodmi/randomMindustry/Main.java
+++ src/com/gorodmi/randomMindustry/Main.java
@@ -7,17 +7,12 @@ import mindustry.game.EventType.*;
 import mindustry.mod.*;
 import mindustry.type.*;
 
-import com.gorodmi.randomMindustry.*;
-import com.gorodmi.randomMindustry.content.*;
-
 import static arc.Core.*;
 import static mindustry.Vars.*;
 
 public class Main extends Mod{
     public Main(){
         Events.on(ClientLoadEvent.class, (e) -> {
-            RMSettings.load();
-            
             ResourceMapper.init();
             BlockMapper.init();
             for (ItemPack pack : ResourceMapper.itemMap.values()) {
