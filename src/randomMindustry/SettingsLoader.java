4f1f2d7eb4e639e42f70360726edfd61890e2d28
Mythril382 <77225817+Mythril382@users.noreply.github.com>
2022-06-24 21:38:09 +0800

    Create SettingsLoader.java

---
+++ src/randomMindustry/SettingsLoader.java
@@ -0,0 +1,18 @@
+package randomMindustry.content;
+
+import mindustry.gen.*;
+import mindustry.ui.dialogs.*;
+
+import static mindustry.Vars.*;
+
+public class SettingsLoader{
+    public static void init(){
+        SettingsMenuDialog dialog = ui.settings;
+        
+        dialog.addCategory("@setting.rm", Icon.effect /* TODO */, c -> {
+            c.checkPref("rm-ammo", true);
+            c.checkPref("rm-block-output", true);
+            c.checkPref("rm-block-input", true);
+        });
+    }
+}
