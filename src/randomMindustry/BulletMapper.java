1b532fc5a5cb2bae8138034ffa6f4d51f666e77a
Mythril382 <77225817+Mythril382@users.noreply.github.com>
2022-06-27 11:03:42 +0800

    bulletmapper overhaul

--- src/randomMindustry/BulletMapper.java
+++ src/randomMindustry/BulletMapper.java
@@ -5,10 +5,6 @@ import static mindustry.Vars.*;
 public class BulletMapper{
     // TODO: Apply settings.
     public static void init(){
-        /*content.units().each(u -> {
-            u.weapons.each(w -> {
-                w.bullet = content.bullets().random(Main.rand);
-            });
-        });*/
+        
     }
 }
