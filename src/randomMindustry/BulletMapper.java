41d1e8355d980a39667f13962f1a7d9aa7f36800
Mythril382 <77225817+Mythril382@users.noreply.github.com>
2022-06-27 11:28:36 +0800

    bullet visuals randomizer

--- src/randomMindustry/BulletMapper.java
+++ src/randomMindustry/BulletMapper.java
@@ -1,10 +1,29 @@
 package randomMindustry;
 
+import arc.struct.*;
+import arc.math.*;
+import arc.assets.loaders.*;
+import mindustry.entities.*;
+import mindustry.entities.bullet.*;
+
 import static mindustry.Vars.*;
 
 public class BulletMapper{
-    // TODO: Apply settings.
     public static void init(){
+        Seq<Effect> effects = Effect.all;
         
+        // Currently only visual, will be fixed later on.
+        content.bullets().each(b -> {
+            b.hitSound = Sounds.getSound(ResourceMapper.getRandomInt(0, 71));
+            b.despawnSound = Sounds.getSound(ResourceMapper.getRandomInt(0, 71));
+            
+            b.shootEffect = effects.random(Main.rand);
+            b.smokeEffect = effects.random(Main.rand);
+            b.chargeEffect = effects.random(Main.rand);
+            b.hitEffect = effects.random(Main.rand);
+            b.despawnEffect = effects.random(Main.rand);
+            b.trailEffect = effects.random(Main.rand);
+            b.healEffect = effects.random(Main.rand);
+        });
     }
 }
