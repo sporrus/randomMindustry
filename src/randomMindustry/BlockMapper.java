64d4716e0cdcd03c61d5baf7426337549b64729f
Mythril382 <77225817+Mythril382@users.noreply.github.com>
2022-06-27 15:55:27 +0800

    fix

--- src/randomMindustry/BlockMapper.java
+++ src/randomMindustry/BlockMapper.java
@@ -46,6 +46,8 @@ public class BlockMapper {
         } else {
             modifyBlock(block);
         }
+        
+        block.init();
     }
 
     public static void modifyTurret(Turret block) {
