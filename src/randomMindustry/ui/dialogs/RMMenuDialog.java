4ef3630046e5c72186433ca81fd22f61466883ba
Mythril382 <77225817+Mythril382@users.noreply.github.com>
2022-06-28 12:00:46 +0800

    first dialog

---
+++ src/randomMindustry/ui/dialogs/RMMenuDialog.java
@@ -0,0 +1,30 @@
+package randomMindustry.ui.dialogs;
+
+import mindustry.ui.dialogs.*;
+
+import static mindustry.Vars.*;
+
+public class RMMenuDialog extends BaseDialog{
+    public RMMenuDialog(){
+        super("@dialog.rm-menu");
+        shouldPause = true;
+        
+        shown(this::rebuild);
+        
+        addCloseButton();
+    }
+    
+    void rebuild(){
+        if(!mobile){
+            cont.defaults().width(220f).height(55).pad(5f);
+            
+            cont.button("@rm-credits", Icon.info, () -> {}).row();
+            cont.button("@rm-item-finder", Icon.search, () -> {}).row();
+        }else{
+            cont.defaults().size(130f).pad(5);
+            
+            cont.buttonRow("@rm-credits", Icon.info, () -> {}).row();
+            cont.buttonRow("@rm-item-finder", Icon.search, () -> {}).row();
+        }
+    }
+}
