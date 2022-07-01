aa42cc5b594d714350806e97eca799445064b781
Mythril382 <77225817+Mythril382@users.noreply.github.com>
2022-07-01 18:17:05 +0800

    oh lordy

--- src/randomMindustry/random/mappers/BulletMapper.java
+++ src/randomMindustry/random/mappers/BulletMapper.java
@@ -17,9 +17,11 @@ public class BulletMapper{
         Seq<BulletType> bullets = content.bullets();
         RandomUtil.shuffle(bullets);
         bullets.each(bullet -> {
+            // sfx
             bullet.hitSound = Sounds.getSound(RandomUtil.getRand().random(0, 71));
             bullet.despawnSound = Sounds.getSound(RandomUtil.getRand().random(0, 71));
             
+            // fx
             bullet.hitEffect = effects.random(RandomUtil.getRand());
             bullet.despawnEffect = effects.random(RandomUtil.getRand());
             bullet.shootEffect = effects.random(RandomUtil.getRand());
@@ -28,14 +30,31 @@ public class BulletMapper{
             bullet.trailEffect = effects.random(RandomUtil.getRand());
             bullet.healEffect = effects.random(RandomUtil.getRand());
             
+            // weavin 
             bullet.weaveScale = RandomUtil.getRand().random(0.5f, 20f);
             bullet.weaveMag = RandomUtil.getRand().random(0.5f, 10f);
             
+            // other stats
             bullet.rangeChange = RandomUtil.getRand().random(0.5f, 20f);
             bullet.scaleLife = RandomUtil.getRand().random(-2f, 2f) < 0f ? true : false;
             bullet.healPercent = RandomUtil.getRand().random(0f, 100f);
             if(bullet.healPercent > 0) bullet.collidesTiles = bullet.collidesTeam = bullet.collides = true;
-            bullet.lifetime = RandomUtil.getRand().random(bullet.lifetime + (bullet.lifetime / 2));
+            bullet.lifetime = RandomUtil.getRand().random(bullet.lifetime + (bullet.lifetime / 2f));
+            bullet.recoil = RandomUtil.getRand().random(-25f, 25f);
+            bullet.knockback = RandomUtil.getRand().random(-10f, 10f);
+            bullet.hitShake = RandomUtil.getRand().random(-10f, 10f);
+            bullet.despawnShake = RandomUtil.getRand().random(-10f, 10f);
+            
+            // size and rotation
+            bullet.width = RandomUtil.getRand().random(bullet.width + (bullet.width / 2f));
+            bullet.height = RandomUtil.getRand().random(bullet.height + (bullet.height / 2f));
+            bullet.shrinkX = RandomUtil.getRand().random(-1f, 1f);
+            bullet.shrinkY = RandomUtil.getRand().random(-1f, 1f);
+            bullet.spin = RandomUtil.getRand().random(45f);
+            
+            // status effect 
+            bullet.status = content.statusEffects().random(RandomUtil.getRand());
+            bullet.statusDuration = RandomUtil.getRand().random(600f);
         });
     }
 }
