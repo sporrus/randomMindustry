7854e497baf8601abef987e38fa009db2d3f1632
Mythril382 <77225817+Mythril382@users.noreply.github.com>
2022-07-01 11:35:00 +0800

    Create BulletMapper.java

---
+++ src/randomMindustry/random/mappers/BulletMapper.java
@@ -0,0 +1,49 @@
+package randomMindustry.random.mappers;
+
+import arc.struct.*;
+import mindustry.gen.*;
+import mindustry.content.*;
+import mindustry.entities.*;
+import mindustry.entities.bullet.*;
+
+import randomMindustry.random.util.*;
+
+import static mindustry.Vars.*;
+
+public class BulletMapper{
+    public static void init(){
+        Seq<Effect> effects = Effect.all.select(effect -> effect != Fx.dynamicExplosion);
+        
+        Seq<BulletType> bullets = content.bullets();
+        RandomUtil.shuffle(bullets);
+        bullets.each(bullet -> {
+            // avoid recursion
+            Seq<BulletType> bulletSeq = bullets.select(b -> b != bullet);
+            
+            bullet.hitSound = Sounds.getSound(RandomUtil.getRand().random(0, 71));
+            bullet.despawnSound = Sounds.getSound(RandomUtil.getRand().random(0, 71));
+            
+            bullet.hitEffect = effects.random(RandomUtil.getRand());
+            bullet.despawnEffect = effects.random(RandomUtil.getRand());
+            bullet.shootEffect = effects.random(RandomUtil.getRand());
+            bullet.chargeEffect = effects.random(RandomUtil.getRand());
+            bullet.smokeEffect = effects.random(RandomUtil.getRand());
+            bullet.trailEffect = effects.random(RandomUtil.getRand());
+            bullet.healEffect = effects.random(RandomUtil.getRand());
+            
+            bullet.fragBullet = bulletSeq.random(RandomUtil.getRand());
+            bullet.fragBullets = RandomUtil.getRandomIntMult(2, 20, 2);
+            
+            bullet.intervalBullet = bulletSeq.random(RandomUtil.getRand());
+            bullet.bulletInterval = RandomUtil.getRand().random(1f, 100f);
+            bullet.intervalBullets = RandomUtil.getRand().random(2, 10);
+            
+            bullet.weaveScale = RandomUtil.getRand().random(0.5f, 20f);
+            bullet.weaveMag = RandomUtil.getRand().random(0.5f, 10f);
+            
+            bullet.rangeChange = RandomUtil.getRand().random(0.5f, 20f);
+            bullet.scaleLife = RandomUtil.getRand().random(-2f, 2f) < 0f ? true : false;
+            bullet.healPercent = RandomUtil.getRand().random(0f, 100f);
+        });
+    }
+}
