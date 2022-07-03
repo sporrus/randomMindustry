a848da200aaf0291716e52011e1a32c2a70832bf
Mythril382 <77225817+Mythril382@users.noreply.github.com>
2022-07-03 21:23:49 +0800

    g

--- src/randomMindustry/random/mappers/BulletMapper.java
+++ src/randomMindustry/random/mappers/BulletMapper.java
@@ -18,6 +18,8 @@ public class BulletMapper {
         Seq<BulletType> bullets = content.bullets().copy();
         RandomUtil.shuffle(bullets);
         bullets.each(bullet -> {
+            Seq<BulletType> bulletSeq = content.bullets().select(b -> b != bullet);
+            
             bullet.hitSound = Util.generateSound();
             bullet.despawnSound = Util.generateSound();
 
@@ -49,19 +51,26 @@ public class BulletMapper {
             bullet.damage = RandomUtil.getRand().random(1f, 100f);
             bullet.buildingDamageMultiplier = RandomUtil.getRand().random(0f, 2f);
 
-            if (bullet instanceof BasicBulletType basicBulletType) {
-                basicBulletType.width = RandomUtil.getRand().random(4f, 20f);
-                basicBulletType.height = RandomUtil.getRand().random(4f, 20f);
-                basicBulletType.shrinkX = RandomUtil.getRand().random(-1f, 1f);
-                basicBulletType.shrinkY = RandomUtil.getRand().random(-1f, 1f);
-                basicBulletType.spin = RandomUtil.getRand().random(45f);
-                basicBulletType.shrinkX = RandomUtil.getRand().random(-1f, 1f);
-                basicBulletType.shrinkY = RandomUtil.getRand().random(-1f, 1f);
-                basicBulletType.spin = RandomUtil.getRand().random(45f);
+            if (bullet instanceof BasicBulletType basicBullet) {
+                basicBullet.width = RandomUtil.getRand().random(4f, 20f);
+                basicBullet.height = RandomUtil.getRand().random(4f, 20f);
+                basicBullet.shrinkX = RandomUtil.getRand().random(-1f, 1f);
+                basicBullet.shrinkY = RandomUtil.getRand().random(-1f, 1f);
+                basicBullet.spin = RandomUtil.getRand().random(45f);
+                basicBullet.shrinkX = RandomUtil.getRand().random(-1f, 1f);
+                basicBullet.shrinkY = RandomUtil.getRand().random(-1f, 1f);
+                basicBullet.spin = RandomUtil.getRand().random(45f);
             }
  
             bullet.status = content.statusEffects().random(RandomUtil.getRand());
             bullet.statusDuration = RandomUtil.getRand().random(600f);
+            
+            bullet.fragBullets = RandomUtil.getRand().random(1, 20);
+            bullet.fragBullet = RandomUtil.getRand().random(-5f, 5f) < 0f ? null : bulletSeq.random();
+            
+            bullet.intervalBullets = RandomUtil.getRand().random(2, 10);
+            bullet.intervalBullet = RandomUtil.getRand().random(-5f, 5f) < 0f ? null : bulletSeq.random();
+            bullet.bulletInterval = RandomUtil.getRand().random(5f, 600f);
         });
     }
 }
