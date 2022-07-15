a572d9fb3e7a60cef19bc7137231c9a15fbbcbdd
gorodmi <gorodmibot@gmail.com>
2022-07-15 13:42:34 +0300

    uhhh t

--- src/randomMindustry/random/mappers/UnitMapper.java
+++ src/randomMindustry/random/mappers/UnitMapper.java
@@ -22,8 +22,8 @@ public class UnitMapper{
     }
     
     public static void modify(UnitType unit){
-        Seq<Effect> effects = Effect.all.select(effect -> effect != Fx.dynamicExplosion);
-        
+        Seq<Effect> effects = FxMapper.effects;
+
         if(!unit.flying && !unit.naval){
             unit.canBoost = RandomUtil.getRand().random(-2f, 2f) > 0;
             unit.boostMultiplier = RandomUtil.getRand().random(5f);
@@ -34,7 +34,7 @@ public class UnitMapper{
         unit.createScorch = RandomUtil.getRand().random(-2f, 2f) > 0;
         
         unit.legCount = RandomUtil.getRandomIntMult(2, 10, 2);
-        unit.legGroupSize = RandomUtil.getRandomIntMult(2, 10, 2);
+        unit.legGroupSize = unit.legCount / RandomUtil.getRand().random(1, 3);
         unit.legLength = RandomUtil.getRand().random(-70f, 70f);
         unit.legSpeed = RandomUtil.getRand().random(0.1f, 3f);
         unit.legStraightness = RandomUtil.getRand().random(1f);
@@ -44,56 +44,11 @@ public class UnitMapper{
         unit.deathSound = Util.generateSound();
         unit.mineSound = Util.generateSound();
         
-        unit.fallEffect = effects.random(RandomUtil.getRand());
-        unit.fallEngineEffect = effects.random(RandomUtil.getRand());
-        unit.deathExplosionEffect = effects.random(RandomUtil.getRand());
+        unit.fallEffect = effects.random(RandomUtil.getClientRand());
+        unit.fallEngineEffect = effects.random(RandomUtil.getClientRand());
+        unit.deathExplosionEffect = effects.random(RandomUtil.getClientRand());
         
         if(unit instanceof MissileUnitType) return;
-        unit.weapons.each(weapon -> {
-            weapon.shootSound = Util.generateSound();
-            weapon.chargeSound = Util.generateSound();
-            weapon.noAmmoSound = Util.generateSound();
-            weapon.reload = RandomUtil.getRand().random(100f);
-            weapon.shootCone = RandomUtil.getRand().random(1f, 360f);
-            weapon.inaccuracy = RandomUtil.getRand().random(1f, 180f);
-            weapon.xRand = RandomUtil.getRand().random(1f, 10f);
-            weapon.shootX = RandomUtil.getRand().random(-20f, 20f);
-            weapon.shootY = RandomUtil.getRand().random(-20f, 20f);
-            weapon.x = RandomUtil.getRand().random(-30f, 30f);
-            weapon.y = RandomUtil.getRand().random(-30f, 30f);
-            weapon.recoil = RandomUtil.getRand().random(-50f, 50f);
-            weapon.rotateSpeed = RandomUtil.getRand().random(1f, 20f);
-            weapon.rotationLimit = RandomUtil.getRand().random(45f, 361f);
-            
-            weapon.bullet = content.bullets().random(RandomUtil.getRand());
-            
-            int pattern = RandomUtil.getRand().random(0, 5);
-            switch (pattern) {
-                case 0 -> weapon.shoot = new ShootPattern();
-                case 1 -> weapon.shoot = new ShootAlternate(RandomUtil.getRand().random(1f, 15f)) {{
-                    barrels = RandomUtil.getRand().random(1, 5);
-                }};
-                case 2 -> weapon.shoot = new ShootBarrel() {{
-                    int barrelAmount = RandomUtil.getRand().random(1, 10);
-                    float[] barrelArray = new float[barrelAmount * 3];
-                    for (int i = 0; i < barrelArray.length; i += 3) {
-                        barrelArray[i] = RandomUtil.getRand().random(-10f, 10f);
-                        barrelArray[i + 1] = RandomUtil.getRand().random(-10f, 10f);
-                        barrelArray[i + 2] = RandomUtil.getRand().random(0, 360f);
-                    }
-                    barrels = barrelArray;
-                }};
-                case 3 -> weapon.shoot = new ShootHelix() {{
-                    scl = RandomUtil.getRand().random(1f, 20f);
-                    mag = RandomUtil.getRand().random(1f, 10f);
-                }};
-                case 4 -> weapon.shoot = new ShootSine(RandomUtil.getRand().random(1f, 20f), RandomUtil.getRand().random(1f, 10f));
-                case 5 -> weapon.shoot = new ShootSpread() {{
-                    spread = RandomUtil.getRand().random(1f, 15f);
-                }};
-            }
-            weapon.shoot.shots = RandomUtil.getRand().random(1, 10);
-            weapon.shoot.shotDelay = RandomUtil.getRand().random(1f, 60f);
-        });
+        unit.weapons.each(WeaponMapper::map);
     }
 }
