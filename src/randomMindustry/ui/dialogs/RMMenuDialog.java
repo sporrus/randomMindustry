e7f0ee10a9f0bf6351dc5d065b14f67ba26bde3c
Mythril382 <77225817+Mythril382@users.noreply.github.com>
2022-07-20 04:39:24 +0800

    hm

--- src/randomMindustry/ui/dialogs/RMMenuDialog.java
+++ src/randomMindustry/ui/dialogs/RMMenuDialog.java
@@ -1,9 +1,16 @@
 package randomMindustry.ui.dialogs;
 
+import arc.math.*;
+import arc.util.*;
+import arc.scene.ui.*;
+import arc.scene.event.*;
+import arc.scene.actions.*;
 import mindustry.gen.*;
+import mindustry.ui.*;
 import mindustry.ui.dialogs.*;
 
 import static mindustry.Vars.*;
+import static arc.Core.*;
 
 public class RMMenuDialog extends BaseDialog{
     public RMMenuDialog(){
@@ -18,15 +25,29 @@ public class RMMenuDialog extends BaseDialog{
     
     void rebuild(){
         cont.clear();
-
+        
         if(!mobile){
             cont.defaults().width(220f).height(55).pad(5f);
             cont.button("@rm-item-finder", Icon.zoom, Dialogs.itemFinderDialog::show).row();
             cont.button("@rm-servers", Icon.host, Dialogs.serversDialog::show).row();
+            if(settings.getBool("rm-book-collected", false)) cont.button("@rm-book", Icon.book, () -> ui.showInfo("@msg.rm-lever-hunt")).row();
         }else{
             cont.defaults().size(130f).pad(5);
             cont.buttonRow("@rm-item-finder", Icon.zoom, Dialogs.itemFinderDialog::show);
             cont.buttonRow("@rm-servers", Icon.host, Dialogs.serversDialog::show);
+            if(settings.getBool("rm-book-collected", false)){
+                cont.row();
+                cont.buttonRow("@rm-book", Icon.book, () -> ui.showInfo("@msg.rm-lever-hunt"));
+            }
+        }
+        
+        if(!settings.getBool("rm-book-collected", false)){
+            ImageButton book = cont.button(Icon.book, Styles.emptyi).top().left().size(40f).get();
+            book.clicked(() -> {
+                settings.put("rm-book-collected", true);
+                book.actions(Actions.moveBy(0f, 50f, 0.5f, Interp.pow2In));
+                Time.runTask(30f, this::rebuild);
+            });
         }
     }
 }
