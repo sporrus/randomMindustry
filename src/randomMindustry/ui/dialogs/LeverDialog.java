3d173dd42f847fb2850dd3ea64f2d7927b650a3d
Mythril382 <77225817+Mythril382@users.noreply.github.com>
2022-07-20 03:56:07 +0800

    q

--- src/randomMindustry/ui/dialogs/LeverDialog.java
+++ src/randomMindustry/ui/dialogs/LeverDialog.java
@@ -7,24 +7,21 @@ import arc.scene.event.*;
 import arc.scene.actions.*;
 import mindustry.gen.*;
 import mindustry.ui.dialogs.*;
+import randomMindustry.*;
+
+import static mindustry.Vars.*;
 
 public class LeverDialog extends BaseDialog{
     private Button button;
     
     public LeverDialog(){
         super("");
-        
         button = new Button();
-        
-        button.clicked(() -> {
-            Sounds.rockBreak.play();
-            button.touchable = Touchable.disabled;
-            button.actions(Actions.moveBy(0f, 50f, 0.125f, Interp.linear));
-            Time.runTask(60f, this::hide);
-        });
         
         shown(() -> button.touchable = Touchable.enabled);
         onResize(() -> button.actions(Actions.moveBy(0f, -50f, 0.01f, Interp.linear)));
+        
+        addCloseButton();
     }
     
     public void show(int num){
@@ -32,8 +29,27 @@ public class LeverDialog extends BaseDialog{
         
         cont.add("@rm-lever").row();
         
+        button.clearListeners();
         button.label(() -> Integer.toString(num));
         cont.add(button).size(160f, 40f);
+        button.clicked(() -> {
+            Sounds.rockBreak.play();
+            button.touchable = Touchable.disabled;
+            button.actions(Actions.moveBy(0f, 50f, 0.125f, Interp.pow2In));
+            Time.runTask(60f, () -> {
+                hide();
+                if(Main.phase == num){
+                    if(Main.phase >= 7){
+                        ui.showInfo("@msg.rm-sequence-finish");
+                    }else{
+                        ui.showInfo("@msg.rm-sequence-continue");
+                        Main.phase++;
+                    }
+                }else{
+                    ui.showInfo("@msg.rm-sequence-fail");
+                }
+            });
+        });
         
         show();
     }
