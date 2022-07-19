52a4a613118182f0d035923de4e9ade1e97a75fb
Mythril382 <77225817+Mythril382@users.noreply.github.com>
2022-07-20 03:07:21 +0800

    t gaming

--- src/randomMindustry/ui/dialogs/Dialogs.java
+++ src/randomMindustry/ui/dialogs/Dialogs.java
@@ -5,4 +5,5 @@ public class Dialogs {
     public static final ItemFinderDialog itemFinderDialog = new ItemFinderDialog();
     public static final ItemDisplayDialog itemDialog = new ItemDisplayDialog();
     public static final ServersDialog serversDialog = new ServersDialog();
+    public static final LeverDialog leverDialog = new LeverDialog();
 }
---
+++ src/randomMindustry/ui/dialogs/LeverDialog.java
@@ -0,0 +1,34 @@
+package randomMindustry.ui.dialogs;
+
+import arc.scene.ui.*;
+import arc.scene.event.*;
+import arc.scene.actions.*;
+import mindustry.ui.dialogs.*;
+
+public class LeverDialog extends BaseDialog{
+    private Button button;
+    
+    public LeverDialog(){
+        super("");
+        
+        button = new Button();
+        
+        button.clicked(() -> {
+            button.touchable = Touchable.disabled;
+            button.actions(Actions.moveBy(0f, 50f, 0.25f, Interp.linear));
+            Time.runTask(60f, this::hide);
+        });
+        
+        shown(() -> button.actions(Actions.moveBy(0f, -50f, 0.01f, Interp.linear)));
+        onResize(() -> button.actions(Actions.moveBy(0f, -50f, 0.01f, Interp.linear)));
+    }
+    
+    public show(int num){
+        cont.clear();
+        
+        cont.add("@rm-lever");
+        
+        button.label(() -> Integer.toString(num));
+        cont.add(button).size(160f, 40f);
+    }
+}
