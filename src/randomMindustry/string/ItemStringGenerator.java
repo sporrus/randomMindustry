a4db04db48f6f41c0f37158de200774de9796e40
Mythril382 <77225817+Mythril382@users.noreply.github.com>
2022-08-05 18:15:15 +0800

    unreal

---
+++ src/randomMindustry/string/ItemStringGenerator.java
@@ -0,0 +1,31 @@
+package randomMindustry.string;
+
+import arc.math.*;
+import arc.struct.*;
+import mindustry.type.*;
+
+public class ItemStringGenerator extends StringGenerator{
+    public String generateName(){
+        StringBuilder out = new StringBuilder();
+        out.append(upperCaseFirst(generateWord(1)));
+        out.append(generateSuffix());
+        if(Mathf.chance(0.5f)) out.append(" " + generateType());
+        return out.toString();
+    }
+    
+    private String generateSuffix(){
+        return Seq.with("ite", "ide", "ium").random();
+    }
+    
+    private String generateType(){
+        return Seq.with(
+            "Alloy", "Fabric", "Compound", "Matter", "Pod", "Cyst",
+            "Cluster", "Crystal", "Sheet", "Mix"
+        ).random();
+    }
+    
+    public String generateDescription(Item item){
+        // talk about stats and stuff
+        return "Casual item description.";
+    }
+}
