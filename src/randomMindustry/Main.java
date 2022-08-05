9c2713228a4f261efded32a653e46d16a6e10875
Mythril382 <77225817+Mythril382@users.noreply.github.com>
2022-08-05 15:02:47 +0800

    arrival 2

--- src/randomMindustry/Main.java
+++ src/randomMindustry/Main.java
@@ -1,12 +1,31 @@
 package randomMindustry;
 
+import arc.*;
+import arc.graphics.g2d.*;
+import arc.struct.*;
+import arc.util.*;
+import mindustry.game.EventType.*;
 import mindustry.mod.*;
+import mindustry.ui.dialogs.*;
 import randomMindustry.block.*;
 import randomMindustry.item.*;
 import randomMindustry.texture.*;
 
 public class Main extends Mod {
+    public static TextureRegion arrival;
+    
     public Main() {
+        Events.on(ClientLoadEvent.class, e -> {
+            Seq<TextureRegion> arrivals = new Seq<>();
+            for(int i = 0; i < 7; i++) {
+                arrivals.add(Core.atlas.find("random-mindustry-arrival" + i));
+            }
+            arrival = arrivals.get(0);
+            Events.run(Trigger.update, () -> {
+                int frame = (int)((Time.globalTike / 5f) % arrivals.size);
+                arrival.set(arrivals.get(frame));
+            });
+        });
     }
 
     @Override
