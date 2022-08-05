ce630d855e5d7e3a6639d212a12ff66ae90509f9
Mythril382 <77225817+Mythril382@users.noreply.github.com>
2022-08-05 17:56:35 +0800

    no way
    its back

---
+++ src/randomMindustry/string/StringGenerator.java
@@ -0,0 +1,34 @@
+package randomMindustry.string;
+
+import arc.struct.*;
+
+public class StringGenerator{
+    public final Seq<String> vowels = Seq.with(
+        "i", "e", "u", "a", "ee", "ea", "ie", "ou", "er", "or", "o", "ir", "ur", "ear", "our", "ar",
+        "al", "au", "ae", "oar", "oo", "ui", "ew", "a", "eigh", "ow", "oa", "oi", "oy", "eer",
+        "ere", "are", "air", "oor"
+    );
+    
+    public final Seq<String> consonants = Seq.with(
+        "b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x",
+        "y", "z"
+    );
+    
+    public final Seq<String> template = Seq.with("cv", "vc", "cvc", "vcv");
+    
+    public String generateSyllable(){
+        String tmp = template.random();
+        StringBuilder out = new StringBuilder();
+        for(int i = 0; i < tmp.length(); i++){
+            if(tmp.charAt(i) == 'c') out.append(consonants.random());
+            else out.append(vowels.random());
+        }
+        return out.toString();
+    }
+    
+    public String generateWord(int size){
+        StringBuilder out = new StringBuilder();
+        for(int i = 0; i < size; i++) out.append(generateSyllable());
+        return out.toString();
+    }
+}
