907de8ef1678a91f5b373d1cdd62ff96a7f3c67d
Mythril382 <77225817+Mythril382@users.noreply.github.com>
2022-09-09 09:53:12 +0800

    color

--- src/randomMindustry/Main.java
+++ src/randomMindustry/Main.java
@@ -79,8 +79,8 @@ public class Main extends Mod {
             generator = new RandomPlanetGenerator();
             meshLoader = () -> new HexMesh(this, 6);
             cloudMeshLoader = () -> new MultiMesh(
-                    new HexSkyMesh(this, 11, 0.15f, 0.13f, 5, new Color().set(Pal.spore).mul(0.9f).a(0.75f), 2, 0.45f, 0.9f, 0.38f),
-                    new HexSkyMesh(this, 1, 0.6f, 0.16f, 5, Color.white.cpy().lerp(Pal.spore, 0.55f).a(0.75f), 2, 0.45f, 1f, 0.41f)
+                    new HexSkyMesh(this, 11, 0.15f, 0.13f, 5, new Color().set(Team.crux.color.cpy()).mul(0.9f).a(0.75f), 2, 0.45f, 0.9f, 0.38f),
+                    new HexSkyMesh(this, 1, 0.6f, 0.16f, 5, Color.white.cpy().lerp(Team.crux.color.cpy(), 0.55f).a(0.75f), 2, 0.45f, 1f, 0.41f)
             );
             launchCapacityMultiplier = 0.5f;
             sectorSeed = 0;
@@ -96,12 +96,12 @@ public class Main extends Mod {
                 r.attributes.clear();
                 r.showSpawns = true;
             };
-            atmosphereColor = Pal.techBlue.cpy();
+            atmosphereColor = Team.crux.color.cpy().a(0.65f);
             atmosphereRadIn = 0.02f;
             atmosphereRadOut = 0.3f;
             startSector = 1;
             alwaysUnlocked = true;
-            landCloudColor = Pal.spore.cpy().a(0.5f);
+            landCloudColor = Team.crux.color.cpy().a(0.5f);
             hiddenItems.addAll(Vars.content.items()).removeAll(ItemMapper.generatedItems);
         }};
     }
