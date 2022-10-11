80a54a2cdbb401cafedf2d1ecf297a7a6921912c
kapzduke <60543005+kapzduke@users.noreply.github.com>
2022-10-11 19:26:20 +0300

    Update and rename RandomRouter.java to RandomBridge.java

--- src/randomMindustry/mappers/block/blocks/RandomBridge.java
+++ src/randomMindustry/mappers/block/blocks/RandomBridge.java
@@ -2,25 +2,32 @@ package randomMindustry.mappers.block.blocks;
 
 import arc.math.Mathf;
 import mindustry.type.Category;
-import mindustry.world.blocks.distribution.Router;
+import mindustry.world.blocks.distribution.ItemBridge;
 import randomMindustry.RMVars;
 import randomMindustry.mappers.item.ItemMapper;
 
 import static randomMindustry.mappers.block.BlockMapper.r;
 
-public class RandomConveyor extends Router implements RandomBlock {
+public class RandomItemBridge extends ItemBridge implements RandomBlock {
     public static int lastTier = 1;
     int tier = lastTier++;
 
-    public RandomConveyor(String name) {
+    public RandomItemBridge(String name) {
         super(name);
         size = 1;
         health = Mathf.round(r.random(1, 5) * tier, 5);
+        fadeIn = moveArrows = false
 
         requirements(Category.production, ItemMapper.getItemStacks(tier - 1, r.random(1, 2), () -> r.random(1, 5)));
         stats.add(RMVars.seedStat, RMVars.seedStatValue);
 
-        localizedName = "unreal router name";
-        description = "unreal router description";
+        range = 0.03f * (tier * ItemMapper.maxTier / 12f);
+        
+        if (tier >= 2) {
+           fadeIn = moveArrows = true
+        }
+
+        localizedName = "unreal item bridge name";
+        description = "unreal item bridge description";
     }
 }
