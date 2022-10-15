f9fe60ed6adb9f4fde95cd9bf114f943d1039754
gorodmi <gorodmibot@gmail.com>
2022-10-15 18:05:34 +0300

    roter 2

--- src/randomMindustry/mappers/block/blocks/RandomRouter.java
+++ src/randomMindustry/mappers/block/blocks/RandomRouter.java
@@ -1,11 +1,14 @@
 package randomMindustry.mappers.block.blocks;
 
+import arc.*;
 import arc.graphics.*;
 import arc.graphics.g2d.*;
 import arc.math.*;
 import arc.struct.*;
+import mindustry.content.*;
 import mindustry.graphics.*;
 import mindustry.type.*;
+import mindustry.world.*;
 import mindustry.world.blocks.distribution.*;
 import randomMindustry.*;
 import randomMindustry.mappers.item.*;
@@ -14,7 +17,7 @@ import randomMindustry.texture.*;
 import static randomMindustry.mappers.block.BlockMapper.*;
 import static randomMindustry.RMVars.*;
 
-public class RandomRouter extends Router implements RandomBlock {
+public class RandomRouter extends DuctRouter implements RandomBlock {
     public static int lastTier = 1;
     public Item mainItem;
     public int tier = lastTier++;
@@ -24,11 +27,11 @@ public class RandomRouter extends Router implements RandomBlock {
         size = 1;
         health = Mathf.round(r.random(3, 8) * tier, 1);
 
-        requirements(Category.distribution, ItemMapper.getItemStacks(tier - 1, r.random(1, 3), () -> 1));
+        requirements(Category.distribution, ItemMapper.getItemStacks(tier - 1, r.random(1, 3), () -> 3));
         mainItem = Seq.with(requirements).sort((a, b) -> ((CustomItem) b.item).globalTier - ((CustomItem) a.item).globalTier).get(0).item;
         stats.add(RMVars.seedStat, RMVars.seedStatValue);
 
-        speed = 5.5f * tier;
+        speed = 16f / tier;
         squareSprite = false;
 
         localizedName = mainItem.localizedName + " Router";
@@ -42,6 +45,7 @@ public class RandomRouter extends Router implements RandomBlock {
         super.load();
         if (!pixmapLoaded) return;
         region = fullIcon = uiIcon = pixmapRegion;
+        topRegion = Core.atlas.find("empty");
     }
 
     private TextureRegion pixmapRegion;
