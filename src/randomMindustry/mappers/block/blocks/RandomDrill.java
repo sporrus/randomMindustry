b49843fc7f18832c5e6043665eeda9c9dff049eb
Mythril382 <77225817+Mythril382@users.noreply.github.com>
2022-10-15 21:07:53 +0800

    pog

--- src/randomMindustry/mappers/block/blocks/RandomDrill.java
+++ src/randomMindustry/mappers/block/blocks/RandomDrill.java
@@ -30,7 +30,7 @@ public class RandomDrill extends Drill implements RandomBlock {
         mainItem = Seq.with(requirements).sort((a, b) -> ((CustomItem) b.item).globalTier - ((CustomItem) a.item).globalTier).get(0).item;
         stats.add(RMVars.seedStat, RMVars.seedStatValue);
 
-        localizedName = mainItem + " drill";
+        localizedName = mainItem + " Drill";
     }
 
     @Override
