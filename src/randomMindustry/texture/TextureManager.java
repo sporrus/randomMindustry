4e42e6edb63cd6893bb08ceb6816a1b02b32bdd8
nuri (smol) <75618732+SMOLKEYS@users.noreply.github.com>
2022-10-16 21:10:22 +0800

    kvsus

--- src/randomMindustry/texture/TextureManager.java
+++ src/randomMindustry/texture/TextureManager.java
@@ -33,7 +33,7 @@ public class TextureManager {
     }
 
     public static void reload() {
-        pages.each(e -> e.value.reload());
+        pages.each((k, v) -> v.reload());
     }
 
     public static Seq<Texture> getAllTextures() {
