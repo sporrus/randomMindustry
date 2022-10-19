ad7d369553209d97a02fbc6f7361e2fa15dc848f
Mythril382 <77225817+Mythril382@users.noreply.github.com>
2022-10-19 08:28:01 +0800

    aaaaaaaaa

--- src/randomMindustry/mappers/block/blocks/RandomCore.java
+++ src/randomMindustry/mappers/block/blocks/RandomCore.java
@@ -7,6 +7,7 @@ import arc.struct.*;
 import mindustry.content.*;
 import mindustry.graphics.*;
 import mindustry.type.*;
+import mindustry.world.*;
 import mindustry.world.blocks.storage.*;
 import randomMindustry.*;
 import randomMindustry.mappers.block.*;
@@ -63,8 +64,8 @@ public class RandomCore extends CoreBlock implements RandomBlock{
         size = id + 3;
         itemCapacity = Mathf.round((3000 * getTier()) + r.random(150, 550), 5);
         // not using last because its only used after blocks and other content have loaded
-        RandomBlock lastBlock = BlockMapper.generatedBlocks.find(b -> b instanceof RandomCore && b.id == this.id - 1);
-        requirements(Category.effect, ItemMapper.getItemStacks(getTier() - 1, r.random(3, 5), () -> Math.max(Mathf.round(r.random(300, 1000) * size, 100), (lastBlock == null ? itemCapacity : ((Block)lastBlock).itemCapacity) - 100), r));
+        RandomBlock lastBlock = BlockMapper.generatedBlocks.find(b -> b instanceof RandomCore && ((RandomCore)b).id == this.id - 1);
+        requirements(Category.effect, ItemMapper.getItemStacks(getTier() - 1, r.random(3, 5), () -> ((int)Math.max(Mathf.round(r.random(300, 1000) * size, 100), (lastBlock == null ? itemCapacity : ((Block)lastBlock).itemCapacity) - 100)), r));
         unitType = types.get(id, UnitTypes.oct);
         health = Mathf.round(r.random(1000, 3000) * size, 100);
         armor = id * 2;
