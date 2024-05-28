8801133a0b0fcd9b62fc9d5b1434472f1329b62f
Mythril382 <77225817+Mythril382@users.noreply.github.com>
2024-05-28 23:52:09 +0800

    yeah

--- src/randomMindustry/string/PlanetStringGenerator.java
+++ src/randomMindustry/string/PlanetStringGenerator.java
@@ -3,7 +3,7 @@ package randomMindustry.string;
 public class PlanetStringGenerator extends StringGenerator{
     public String generateName(){
         StringBuilder out = new StringBuilder();
-        out.append(upperCaseFirst(generateWord(r.random(2, 3))));
+        out.append(upperCaseFirst(generateWord(r.random(2))));
         return out.toString();
     }
 }
