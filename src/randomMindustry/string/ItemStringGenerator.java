6314b5244b2332b1cdb3eccb8c57034b00ea6f11
Mythril382 <77225817+Mythril382@users.noreply.github.com>
2024-05-28 21:11:50 +0800

    real

--- src/randomMindustry/string/ItemStringGenerator.java
+++ src/randomMindustry/string/ItemStringGenerator.java
@@ -6,37 +6,41 @@ import randomMindustry.mappers.item.*;
 public class ItemStringGenerator extends StringGenerator{
     public String generateName(){
         StringBuilder out = new StringBuilder();
-        out.append(upperCaseFirst(generateWord(r.random(1, 2))));
-        out.append(generateSuffix());
-        if (r.chance(0.5)) out.append(" ").append(generateType());
+        out.append(upperCaseFirst(generateWord(2)));
+        if(r.chance(0.5f)){
+            String last = out.toString().charAt(out.toString().length - 1);
+            if(vowel(last)){
+                String append = consonants.copy().removeAll(avoidConsonants.get(last));
+                out.append(append);
+            }
+            out.append(generateSuffix());
+        }
+        if(r.chance(0.5f)) out.append(" ").append(generateType());
         return out.toString();
     }
 
     public String generateDescription(CustomItem item){
-        return "Used in " + (r.chance(0.5) ? "advanced " : "") + Seq.with(
-                "weaponry", "defense structures", "electronics", "bombs", "transportation",
-                "units", "ammunition", "drills", "factories").random(r) + "." +
-                " " + fraction(item.explosiveness) + " explosive." +
-                " " + fraction(item.radioactivity) + " radioactive." +
-                " " + fraction(item.flammability) + " flammable." +
-                " " + fraction(item.charge) + " conductive.";
+        return fraction(item.explosiveness) + " explosive." +
+            " " + fraction(item.radioactivity) + " radioactive." +
+            " " + fraction(item.flammability) + " flammable." +
+            " " + fraction(item.charge) + " conductive.";
     }
     
-    private String generateSuffix(){
+    public String generateSuffix(){
         return Seq.with("ite", "ide", "ium", "ate", "unch").random(r);
     }
     
-    private String generateType(){
+    public String generateType(){
         return Seq.with(
                 "Alloy", "Fabric", "Matter", "Pod", "Cyst", "Cluster",
                 "Crystal", "Sheet", "Mix", "Lattice", "Compound", "Plating",
-                "Sheet", "Ingot", "Berry", "Metal", "Foil", "Block"
+                "Sheet", "Ingot", "Bar", "Metal", "Foil", "Block"
         ).random(r);
     }
 
-    private String fraction(float frac) {
+    public String fraction(float frac) {
         return switch ((int) (frac * 6)) {
-            case 0 -> "Not"; //skip instead
+            case 0 -> "Not";
             case 1 -> "Slightly";
             case 2 -> "Sizeably";
             case 3 -> "Moderately";
