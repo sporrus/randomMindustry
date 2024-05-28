84ad8871ccf5eaac647eb599a9a355b8fbf085b5
Mythril382 <77225817+Mythril382@users.noreply.github.com>
2024-05-28 16:12:05 +0800

    put it off

--- src/randomMindustry/string/ItemStringGenerator.java
+++ src/randomMindustry/string/ItemStringGenerator.java
@@ -19,7 +19,7 @@ public class ItemStringGenerator extends StringGenerator{
                 " " + fraction(item.explosiveness) + " explosive." +
                 " " + fraction(item.radioactivity) + " radioactive." +
                 " " + fraction(item.flammability) + " flammable." +
-                " " + fraction(item.charge) + " conductive. (Inaccurate, Temporary)";
+                " " + fraction(item.charge) + " conductive.";
     }
     
     private String generateSuffix(){
