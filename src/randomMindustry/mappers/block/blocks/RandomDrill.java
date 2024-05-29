49548a3de92f2b20eb2618da609a4f28c507edf6
Mythril382 <77225817+Mythril382@users.noreply.github.com>
2024-05-29 12:45:47 +0800

    ball

--- src/randomMindustry/mappers/block/blocks/RandomDrill.java
+++ src/randomMindustry/mappers/block/blocks/RandomDrill.java
@@ -45,7 +45,7 @@ public class RandomDrill extends Drill implements RandomBlock {
         health = Mathf.round(r.random(20, 100) * getTier() * size, 5);
         drillTime = 600f / tier;
         
-        localizedName = drillStringGen.generate(tier);
+        localizedName = drillStringGen.generateName(tier);
     }
 
     private TextureRegion pixmapRegion;
