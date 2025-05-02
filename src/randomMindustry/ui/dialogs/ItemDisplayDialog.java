92eb90f0e94804f86f58fe0891c91a773281ff91
Mythril382 <77225817+Mythril382@users.noreply.github.com>
2025-05-02 23:16:42 +0800

    begin 149 port

--- src/randomMindustry/ui/dialogs/ItemDisplayDialog.java
+++ src/randomMindustry/ui/dialogs/ItemDisplayDialog.java
@@ -29,15 +29,15 @@ public class ItemDisplayDialog extends ContentDisplayDialog {
                         if (consume instanceof ConsumeItems citems) {
                             ItemStack[] itemStacks = citems.items;
                             for (ItemStack itemStack : itemStacks)
-                                inset.add(new ItemDisplay(itemStack.item, itemStack.amount, false)).padLeft(10);
+                                inset.add(StatValues.displayItem(itemStack.item, itemStack.amount, false)).padLeft(10);
                         }
                     }
                     inset.image(Icon.right).padLeft(10).size(30);
                     if ((genericCrafter).outputItems != null) for (ItemStack itemStack : (genericCrafter).outputItems)
-                        inset.add(new ItemDisplay(itemStack.item, itemStack.amount, false)).padLeft(10);
+                        inset.add(StatValues.displayItem(itemStack.item, itemStack.amount, false)).padLeft(10);
                     if ((genericCrafter).outputLiquids != null)
                         for (LiquidStack liquidStack : (genericCrafter).outputLiquids)
-                            inset.add(new LiquidDisplay(liquidStack.liquid, liquidStack.amount, false)).padLeft(10);
+                            inset.add(StatValues.displayLiquid(liquidStack.liquid, liquidStack.amount, false)).padLeft(10);
                 } else {
                     inset.image(Util.getHardnessIcon(block)).size(30);
                     inset.image(Icon.right).padLeft(10).size(30);
@@ -59,7 +59,7 @@ public class ItemDisplayDialog extends ContentDisplayDialog {
                 if (new Seq<>(block.requirements).contains(itemStack -> itemStack.item == content)) {
                     inset.image(block.uiIcon).size(30);
                     inset.image(Icon.hammer).padLeft(10).padRight(10).size(30);
-                    for (ItemStack itemStack : block.requirements) inset.add(new ItemDisplay(itemStack.item, itemStack.amount, false));
+                    for (ItemStack itemStack : block.requirements) inset.add(StatValues.displayItem(itemStack.item, itemStack.amount, false));
                     inset.row();
                 }
                 if (new Seq<>(block.consumers).contains((consume -> consume instanceof ConsumeItems consumeItems &&
@@ -68,7 +68,7 @@ public class ItemDisplayDialog extends ContentDisplayDialog {
                     inset.image(Icon.left).color(Color.yellow).padLeft(10).padRight(10).size(30);
                     for (Consume consume : block.consumers){
                         if (!(consume instanceof ConsumeItems consumeItem)) continue;
-                        for (ItemStack itemStack : consumeItem.items) inset.add(new ItemDisplay(itemStack.item, itemStack.amount, false));
+                        for (ItemStack itemStack : consumeItem.items) inset.add(StatValues.displayItem(itemStack.item, itemStack.amount, false));
                     }
                     inset.row();
                 }
